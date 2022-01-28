0315c295fce477f94c3f906bd9c6d722d2700c7e
upendrasingh12 <upendrarbt@gmail.com>
2022-01-28 21:06:05 +0530

    Update code
    Delete the fyers connection section.

--- OHLCV_30min_alert.py
+++ OHLCV_30min_alert.py
@@ -18,52 +18,6 @@ import xlsxwriter
 from openpyxl import Workbook
 clist = []
 mydf=pd.DataFrame(columns=["Symbol","High","Low","Time"])
-#import stock3
-
-
-#Here we generate the authorization_code to authorize the app 
-#In visual studio code we have to print the response to get the authorization_code
-try:
-    app_id = open("api_id.txt", "r").read()  #app_id is given by fyers
-except IOError:
-    print("api_id.txt file does not exist")
-
-try:
-    app_secret = open("app_secret.txt", "r").read()  #app_secret is given by fyers   
-except IOError:
-    print("app_secret.txt file does not exist")    
-app_session = accessToken.SessionModel(app_id, app_secret)
-response = app_session.auth()
-#print (response)    #to generate authorization_code remove #
-
-
-
-#Now we generate the access token
-#you have to generate the authorization_code after in active
-#Again we comment the app_session.generate_token(), we copy the token
-try:
-    authorization_code = open("authorization_code.txt", "r").read()   
-except IOError:
-    print("authorization_code.txt file does not exist")
-app_session.set_token(authorization_code)
-app_session.generate_token()
-#print(app_session.generate_token())   #to generate token remove #
-
-#Here we check we connected to the api or not
-#comment the print(fyers) after check
-#you have toh generate the token after in active
-try:
-    token = open("token.txt", "r").read()   
-except IOError:
-    print("app_secret.txt file does not exist")
-is_async = False #(By default False, Change to True for asnyc API calls.))
-fyers = fyersModel.FyersModel(is_async)
-#print(fyers)
-
-#Here we check the profile through the token
-#comment the print so we can't get again and agian profile
-"""profile = fyers.get_profile(token = token)
-print(profile)"""
 
 
 def fileread():
