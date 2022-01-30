b0033c7e83e3af43aebede2b452864d03cdcb492
upendrasingh12 <upendrarbt@gmail.com>
2022-01-31 00:14:01 +0530

    Update

--- AlgoTrading/heikin_ashi_alert_backtesting.py
+++ AlgoTrading/heikin_ashi_alert_backtesting.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 from datetime import date
 import time
 
-client_id = "*****-100"                       # Client_id here refers to APP_ID of the created app
+client_id = "VHPC1DCHNN-100"                       # Client_id here refers to APP_ID of the created app
 op = 0
 cl = 0
 
@@ -19,7 +19,7 @@ def read_csv():
     df = pd.read_csv("StockList.csv")
     timeConvert(df)
     
-def fetchData(time_from,time_to, tim,df):
+def fetchData(time_from,time_to,df):
     for row in df.iterrows():
         global name
         name=row[1][0]
@@ -30,8 +30,8 @@ def fetchData(time_from,time_to, tim,df):
         data1 = fyers.history(
         data = {
         "symbol" : stock,
-        "resolution" : "1",
-        "date_format" : "0",
+        "resolution" : "20",
+        "date_format" : "1",
         "range_from" : time_from ,
         "range_to" : time_to,
         "cont_flag" : "1"
@@ -39,39 +39,22 @@ def fetchData(time_from,time_to, tim,df):
         }
         )
 
-        print("Output ->",data1)
+        val = data1.values()
+        for i in val:
+            print(i)
 
 def timeConvert(df):
-    day = datetime.today() - timedelta(days=2)  #if you want to work on previoius day data
+    day = datetime.today() - timedelta(days=3)  #if you want to work on previoius day data
     # day = datetime.now()  #for current day data
     # print("now ->",previous_day)
 
     format_date = day.strftime("%Y-%m-%d")
-    date_time = format_date + " 09:15:00"
-    pattern = '%Y-%m-%d %H:%M:%S'
-    time_from = int(time.mktime(time.strptime(date_time, pattern)))
-    time_from=str(time_from)
-    print("time_from ->", time_from)
-
-    format_date = day.strftime("%Y-%m-%d")
-    date_time = format_date + " 09:25:00"
-    pattern = '%Y-%m-%d %H:%M:%S'
-    time_to = int(time.mktime(time.strptime(date_time, pattern)))
-    time_to=str(time_to)
-    print("time_to ->", time_to)
-
-    # fetchData(time_from,time_to,df)
-
-    tim=10 # time change time is now 10
+    date_time = format_date
+    time_from=str(date_time)
+    time_to=str(date_time)
     
-    for _ in range(42):
-        
-        fetchData(time_from,time_to, tim,df)
-        time_from=str(int(time_from)+600)
-        time_to=str(int(time_to)+600)
-        time.sleep(600)
 
-
+    fetchData(time_from,time_to,df)
 
 
 # data = {"symbol":"NSE:SBIN-EQ","resolution":"15","date_format":"1","range_from":"2022-01-28","range_to":"2022-01-28","cont_flag":"1"}
