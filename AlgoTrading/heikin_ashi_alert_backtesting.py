e1e43987b812a9e4e600a6843561d6cdc91485fa
upendrasingh12 <upendrarbt@gmail.com>
2022-01-30 23:08:53 +0530

    Update

--- AlgoTrading/heikin_ashi_alert_backtesting.py
+++ AlgoTrading/heikin_ashi_alert_backtesting.py
@@ -14,15 +14,12 @@ cl = 0
 access_token = open("token.txt", "r").read()
 
 fyers = fyersModel.FyersModel(token=access_token,is_async=False,client_id=client_id,log_path="/Users/upendrasingh/Documents/GitHub/Algorithmic-trading/AlgoTrading/")
-v = fyers.get_profile()
-
-print("V ->", v)
 
 def read_csv():
     df = pd.read_csv("StockList.csv")
     timeConvert(df)
     
-def fetchData(time_from,time_to,df):
+def fetchData(time_from,time_to, tim,df):
     for row in df.iterrows():
         global name
         name=row[1][0]
@@ -35,15 +32,11 @@ def fetchData(time_from,time_to,df):
         cashValue=row[1][3]
         print("cashValue ->", cashValue)
 
-        print("TIME_FROM -->", time_from)
-        print("TIME_TO -->", time_to)
-
-        data2 = fyers.get_profile()
         data1 = fyers.history(
         data = {
         "symbol" : stock,
         "resolution" : "10",
-        "date_format" : "1",
+        "date_format" : "0",
         "range_from" : time_from ,
         "range_to" : time_to,
         "cont_flag" : "1"
@@ -51,8 +44,7 @@ def fetchData(time_from,time_to,df):
         }
         )
 
-        print("Output1 ->",data1)
-        print("Output2 ->",data2)
+        print("Output ->",data1)
 
 def timeConvert(df):
     day = datetime.today() - timedelta(days=2)  #if you want to work on previoius day data
@@ -60,19 +52,31 @@ def timeConvert(df):
     # print("now ->",previous_day)
 
     format_date = day.strftime("%Y-%m-%d")
-    date_time = format_date
-    time_from=str(date_time)
-    time_to=str(date_time)
+    date_time = format_date + " 09:25:00"
+    pattern = '%Y-%m-%d %H:%M:%S'
+    time_from = int(time.mktime(time.strptime(date_time, pattern)))
+    time_from=str(time_from)
+    print("time_from ->", time_from)
 
-    fetchData(time_from,time_to,df)
+    format_date = day.strftime("%Y-%m-%d")
+    date_time = format_date + " 09:35:00"
+    pattern = '%Y-%m-%d %H:%M:%S'
+    time_to = int(time.mktime(time.strptime(date_time, pattern)))
+    time_to=str(time_to)
+    print("time_to ->", time_to)
 
-    # tim=10 # time change time is now 10
+    # fetchData(time_from,time_to,df)
+
+    tim=10 # time change time is now 10
     
-    # for _ in range(42):
-    #     fetchData(time_from,time_to,tim,df)
-    #     time_from=str(int(time_from)+600)
-    #     time_to=str(int(time_to)+600)
-    #     time.sleep(1)
+    for _ in range(42):
+        
+        fetchData(time_from,time_to, tim,df)
+        time_from=str(int(time_from)+600)
+        time_to=str(int(time_to)+600)
+        time.sleep(1)
+
+
 
 
 # data = {"symbol":"NSE:SBIN-EQ","resolution":"15","date_format":"1","range_from":"2022-01-28","range_to":"2022-01-28","cont_flag":"1"}
