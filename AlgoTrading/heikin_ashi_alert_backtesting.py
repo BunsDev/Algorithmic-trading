cce883d088f804cf07fbaef7c5af81364122cba7
upendrasingh12 <upendrarbt@gmail.com>
2022-01-31 16:38:58 +0530

    Update

--- AlgoTrading/heikin_ashi_alert_backtesting.py
+++ AlgoTrading/heikin_ashi_alert_backtesting.py
@@ -6,39 +6,40 @@ def read_files(path1, path2):
     Heikin_Ashi(df_StockList, df_stock_data_save)
     
 
+
 def Heikin_Ashi(df_StockList, df_stock_data_save):
-    df_stock_data_save = pd.read_csv("stock_data_save.csv")
-    
-    
+
+
+   
     ################## Heikin Ashi Close Candle ##########################
-    HA_Close = (df_stock_data_save['Open']+ df_stock_data_save['High']+ df_stock_data_save['Low']+df_stock_data_save['Close'])/4.0
-    ha_cl1 = round(HA_Close/ 0.05) * 0.05
-    df_stock_data_save['HA_Close'] = round(ha_cl1, 2)
+    # HA_Close = (df_stock_data_save['Open']+ df_stock_data_save['High']+ df_stock_data_save['Low']+df_stock_data_save['Close'])/4.0
+    # ha_cl1 = round(HA_Close/ 0.05) * 0.05
+    # df_stock_data_save['HA_Close'] = round(ha_cl1, 2)
     # print("Close ->", df_stock_data_save['HA_Close'])
 
 
     ################## Heikin Ashi Open Candle ##########################
-    Open_Previous_Value = df_StockList["HA_Open"]
+    # Open_Previous_Value = df_StockList["HA_Open"]
     # print(len(Stock_Name))
 
     # for i in range(0, len(df_StockList)):
     #     if i <= len(Open_Previous_Value):
     #         print(Open_Previous_Value)
 
-    i = len(Open_Previous_Value)
+    # i = len(Open_Previous_Value)
 
-    while i <= len(Open_Previous_Value):
-        i += 1
-        open = Open_Previous_Value
-        print(open)
+    # while i <= len(Open_Previous_Value):
+    #     i += 1
+    #     open = Open_Previous_Value
+    #     print(open)
 
-    Close_Previous_Value = df_StockList["HA_Close"]
-    # print(len(Stock_Name))
-    i = len(Close_Previous_Value)
-    while i <= len(Close_Previous_Value):
-        i += 1
-        close = Close_Previous_Value
-        print(close)
+    # Close_Previous_Value = df_StockList["HA_Close"]
+    # # print(len(Stock_Name))
+    # i = len(Close_Previous_Value)
+    # while i <= len(Close_Previous_Value):
+    #     i += 1
+    #     close = Close_Previous_Value
+    #     print(close)
     
 
 
