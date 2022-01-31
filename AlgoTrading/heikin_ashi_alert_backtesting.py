86369db8ab49196ba7662afed70866dd5f9f4494
upendrasingh12 <upendrarbt@gmail.com>
2022-01-31 15:33:36 +0530

    update

--- AlgoTrading/heikin_ashi_alert_backtesting.py
+++ AlgoTrading/heikin_ashi_alert_backtesting.py
@@ -2,15 +2,24 @@ from asyncore import read
 import pandas as pd
 
 def read_csv():
-    df = pd.read_csv("stock_data_save.csv")
-    heikin_ashi(df)
+    df_StockList = pd.read_csv("StockList.csv")
+    Heikin_Ashi(df_StockList)
     
 
-def heikin_ashi(df):
-    HA_Close = (df['Open']+ df['High']+ df['Low']+df['Close'])/4.0
+def Heikin_Ashi(df_StockList):
+    df_stock_data_save = pd.read_csv("stock_data_save.csv")
+    
+    
+    ################## Heikin Ashi Close Candle ##########################
+    HA_Close = (df_stock_data_save['Open']+ df_stock_data_save['High']+ df_stock_data_save['Low']+df_stock_data_save['Close'])/4.0
     ha_cl1 = round(HA_Close/ 0.05) * 0.05
-    df['HA_Close'] = round(ha_cl1, 2)
-    print("Close ->", df['HA_Close'])
+    df_stock_data_save['HA_Close'] = round(ha_cl1, 2)
+    # print("Close ->", df_stock_data_save['HA_Close'])
+
+
+    ################## Heikin Ashi Open Candle ##########################
+    
+
 
 
 read_csv()
